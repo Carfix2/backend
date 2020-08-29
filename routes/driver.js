21369300f68c6dd8406c3c8f7c4b95e423c97e7b
devtoohard <e0052482@u.nus.edu>
2020-08-29 15:02:53 +0800

    update user profile

--- routes/driver.js
+++ routes/driver.js
@@ -21,3 +21,44 @@ router.get("/me", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+router.put(
+  "/updateprofile",
+  [
+    auth,
+    [
+      check("email", "email is required").not().isEmpty(),
+      check("phoneNumber", "phone number is required").not().isEmpty(),
+      check("whatsAppNumber", "whatsAppNumber is required").not().isEmpty(),
+      check("telegramId", "telegramId is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, phoneNumber, whatsAppNumber, telegramId } = req.body;
+
+    const newProfile = {
+      email,
+      phoneNumber,
+      whatsAppNumber,
+      telegramId,
+    };
+
+    try {
+      const profile = await Driver.findOne({ Driver: req.params.id });
+      profile.data.unshift(newProfile);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
